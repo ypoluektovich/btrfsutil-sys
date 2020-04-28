338cba6a6d94adab7835fd4b7a2561237dfb45a7
cezarmathe <me@cezarmathe.com>
2020-04-28 20:51:14 +0300

    Fix docs and build issues

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 # btrfsutil-sys
 
+[![btrfsutil-sys](https://img.shields.io/crates/v/btrfsutil-sys)](https://crates.io/crates/btrfsutil-sys)
+[![docs](https://docs.rs/btrfsutil-sys/badge.svg)](https://docs.rs/btrfsutil-sys)
+
 Raw bindings to [libbtrfsutil](https://github.com/kdave/btrfs-progs/tree/master/libbtrfsutil).
-
-## Requirements
-
-- bindgen
-- btrfs-progs
-- clang
-
-
-**Work in progress!**
--- build.rs
+++ build.rs
@@ -1,31 +1,63 @@
-extern crate bindgen;
-
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-    println!("cargo:rustc-link-lib=btrfs");
+use serde::Deserialize;
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("/usr/include/btrfsutil.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
+#[derive(Deserialize)]
+struct BindgenWhitelist {
+    types: Option<Vec<String>>,
+    functions: Option<Vec<String>>,
+    vars: Option<Vec<String>>,
+}
+
+fn build() {
+    println!("cargo:rerun-if-changed=libbtrfsutil/btrfsutil.h");
+    println!("cargo:rerun-if-changed=bindgen_whitelist.toml");
+
+    let bindgen_whitelist_string: String = String::from_utf8(
+        std::fs::read("bindgen_whitelist.toml").expect("Failed to open bindgen_whitelist.toml"),
+    )
+    .expect("Failed to convert the whitelist file to UTF-8 string");
+    let bindgen_whitelist: BindgenWhitelist = toml::from_str(bindgen_whitelist_string.as_str())
+        .expect("Failed to deserialize bindgen whitelist");
+
+    let mut bindings_builder: bindgen::Builder =
+        bindgen::Builder::default().header("libbtrfsutil/btrfsutil.h");
+
+    if let Some(val) = bindgen_whitelist.types {
+        for type_name in val {
+            bindings_builder = bindings_builder.whitelist_type(type_name);
+        }
+    }
+    if let Some(val) = bindgen_whitelist.functions {
+        for function_name in val {
+            bindings_builder = bindings_builder.whitelist_function(function_name);
+        }
+    }
+    if let Some(val) = bindgen_whitelist.vars {
+        for var_name in val {
+            bindings_builder = bindings_builder.whitelist_var(var_name);
+        }
+    }
+    bindings_builder = bindings_builder.parse_callbacks(Box::new(bindgen::CargoCallbacks));
+
+    let bindings: bindgen::Bindings = bindings_builder
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+#[cfg(not(doc))]
+fn main() {
+    build();
+}
+
+#[cfg(doc)]
+fn main() {
+    println!("cargo:rustc-link-lib=btrfsutil");
+    build();
 }
